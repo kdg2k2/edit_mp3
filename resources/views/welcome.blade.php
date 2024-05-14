95d643029b719cd205a533374d3af021b5c44076
kdg2k2 <kdg2k2@gmail.com>
2024-05-14 17:02:55 +0700

    .

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,99 +1,104 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <title>Laravel</title>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+    <link rel="stylesheet" href="/css/app.css">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-            .full-height {
-                height: 100vh;
-            }
+</head>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-1 col-sm-1"></div>
+            <div class="card py-3 my-5 col-lg-6 col-md-10">
+                <input type="file" accept=".mp3" class="form-control" id="fileInput" accept=".mp3">
+                <div id="audio">
+                    <div id="waveform"></div>
                 </div>
             </div>
+            <div class="col-lg-3 col-md-1 col-sm-1"></div>
         </div>
-    </body>
+    </div>
+
+    <script src="/js/app.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/wavesurfer.js/2.0.4/wavesurfer.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/wavesurfer.js/3.2.0/plugin/wavesurfer.timeline.min.js"></script>
+
+    <script>
+        $(document).ready(function () {
+            $('#fileInput').on('change', function () {
+                const file = this.files[0];
+                if (file) {
+                    $('#audio').prepend(`
+                        <input type="range" id="zoom-slider" min="1" max="10" step="1" value="1" class="mt-4">
+                    `)
+                    $('#audio').append(`
+                        <div id="wave-timeline"></div>
+                        <div class="row mt-3">
+                            <div class="col-4"><div id="current-time" class="mt-2">Current Time: 00:00</div></div>
+                            <div class="col-4 text-center"><button id="play">Play/Stop</button></div>
+                            <div class="col-4"><div id="duration" class="mt-2 text-right">Total Duration: 00:00</div></div>
+                        </div>
+                    `);
+
+                    // khởi tạo wave
+                    let wavesurfer = WaveSurfer.create({
+                        container: '#waveform',
+                        waveColor: 'violet',
+                        progressColor: 'purple',
+                        plugins: [
+                            WaveSurfer.timeline.create({
+                                container: '#wave-timeline'
+                            })
+                        ]
+                    });
+
+                    // đọc file và hiển thị wave
+                    const reader = new FileReader();
+                    reader.onload = function (event) {
+                        wavesurfer.load(event.target.result);
+                    };
+                    reader.readAsDataURL(file);
+
+                    // hiển thị thời lượng của file
+                    wavesurfer.on('ready', function () {
+                        let duration = wavesurfer.getDuration();
+                        let minutes = Math.floor(duration / 60);
+                        let seconds = Math.floor(duration % 60);
+                        $('#duration').text(`Total Duration: ${minutes}:${seconds < 10 ? '0' : ''}${seconds}`);
+
+                        // log metadata
+                        console.log(wavesurfer.backend.getPeaks(1000));
+                    });
+
+                    // click chạy audio
+                    $('#play').on('click', function () {
+                        wavesurfer.playPause();
+                    });
+
+                    // hiển thị thời gian chạy
+                    wavesurfer.on('audioprocess', function () {
+                        let currentTime = wavesurfer.getCurrentTime();
+                        let minutes = Math.floor(currentTime / 60);
+                        let seconds = Math.floor(currentTime % 60);
+                        $('#current-time').text(`Current Time: ${minutes}:${seconds < 10 ? '0' : ''}${seconds}`);
+                    });
+
+                    // zoom audio
+                    $('#zoom-slider').on('change', function () {
+                        wavesurfer.zoom(Number($(this).val()) * 10);
+                    });
+                }
+            });
+        });
+    </script>
+</body>
+
 </html>
